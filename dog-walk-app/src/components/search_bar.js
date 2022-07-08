f92e06bc76e8ff66c749f691d162a9a425dda8b2
“Michelle <itsmichellelin@gmail.com>
2022-07-07 23:32:17 -0700

    Basic setup and beginnings

---
+++ dog-walk-app/src/components/search_bar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './search_bar.css';
+
+function Search() {
+    return (
+        <input type="text" placeholder="Where is your next adventure?" className="searchbar"></input>
+    )
+}
+
+export default Search;
